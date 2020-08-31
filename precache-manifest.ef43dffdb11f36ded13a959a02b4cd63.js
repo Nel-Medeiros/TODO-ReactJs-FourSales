ef57a528817ba3696ff3025f58c975aeb788b2ed
Nel-Medeiros <emanuelpaivamedeiros@gmail.com>
2020-08-30 21:19:53 -0300

    Updates

---
+++ precache-manifest.ef43dffdb11f36ded13a959a02b4cd63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cfdc89bb0bb5f9980f0787864d2c325",
+    "url": "/TODO-ReactJs-FourSales/index.html"
+  },
+  {
+    "revision": "188ccd1176e1282e614e",
+    "url": "/TODO-ReactJs-FourSales/static/css/main.979562be.chunk.css"
+  },
+  {
+    "revision": "5f61985d50cfff586af0",
+    "url": "/TODO-ReactJs-FourSales/static/js/2.deb2da1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TODO-ReactJs-FourSales/static/js/2.deb2da1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "188ccd1176e1282e614e",
+    "url": "/TODO-ReactJs-FourSales/static/js/main.e9be6474.chunk.js"
+  },
+  {
+    "revision": "3591c4f2de81db416d73",
+    "url": "/TODO-ReactJs-FourSales/static/js/runtime-main.2bc21035.js"
+  }
+]);
